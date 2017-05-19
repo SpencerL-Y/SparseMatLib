965562f0c83795474df0987a22570d903a94603e
Clexma <1024407215@qq.com>
2017-05-20 05:47:35 +0800

    clexma: CSR

--- src/MatLib/CSRStore/CSRMatrix.cpp
+++ src/MatLib/CSRStore/CSRMatrix.cpp
@@ -39,6 +39,46 @@ void CSRMatrix::createCSRMatrix(unsigned int matWid, unsigned int matHgt)
     matrixWidth = matWid; matrixHeight = matHgt;
 }
 
+unsigned int CSRMatrix::getMatrixWidth() const
+{
+    return matrixWidth;
+}
+
+unsigned int CSRMatrix::getMatrixHeight() const
+{
+    return matrixHeight;
+}
+
+unsigned int CSRMatrix::getMatrixNonZeroNum() const
+{
+    unsigned int i;
+    for(i = 0; i< MAXSIZE && data[i].getColNum()!=0; i++)
+    {
+        ;
+    }
+    return i+1;
+}
+
+void CSRMatrix::insertTupleToMatrix(unsigned int rowNum, CSRTuple ins)
+{
+    int i = rowPtr[rowNum - 1];
+    int j = (rowPtr[rowNum]==0)? MAXSIZE: rowPtr[rowNum];
+    int k;
+    for(k = i; k < j; k++)
+    {
+        if(data[k].getColNum() < ins.getColNum())
+        {
+            break;
+        }
+    }
+    int w;
+    for(w = getMatrixNonZeroNum(); w>=k; w--)
+    {
+        data[w] = data[w-1];
+    }
+
+
+}
 
 
 void CSRMatrix::printMatrix() const
